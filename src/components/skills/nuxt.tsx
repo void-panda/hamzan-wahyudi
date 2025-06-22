e7b8b4227c01bfbcd028c457e0226810d18e157a
Void Panda <hamzankechil@gmail.com>
2025-06-22 17:30:56 +0800

    delete: skills icon react

--- src/components/skills/nuxt.tsx
+++
@@ -1,8 +0,0 @@
-import React, { SVGProps } from 'react'
-
-export function Nuxt(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15" {...props}><g fill="none" stroke="currentColor" strokeLinejoin="round"><path d="m.5 12.5l6-10l6 10z"></path><path d="m4.5 12.5l5-8.5l5 8.5z"></path></g></svg>
-  )
-}
-export default Nuxt
